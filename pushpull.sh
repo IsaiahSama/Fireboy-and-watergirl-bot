25b852b29d76482b7cb621f253505911b67284a1
IsaiahSama <zeldaplayergl@gmail.com>
2021-12-26 21:23:11 -0400

    Changed the way how pushpull works. Will now commit and only push

--- pushpull.sh
+++ pushpull.sh
@@ -4,10 +4,26 @@
 
 array=( $@ )
 len=${#array[@]}
-if [ $len != 1 ]; then
-	echo "Needs exactly one argument, which is the name of the repository"
+if [ $len -lt 2 ]; then
+	echo "Needs a commit message, followed by the name of the repository"
 	exit 1
 fi
 
-git push $1 Rewrite
-git pull $1 Rewrite
+repo=${array[$len-1]}
+msg=${array[@]:0:$len-1}
+
+echo "The commit message is '$msg' and the repo name is '$repo'. Is this ok? y/n"
+read resp
+
+if [ $resp == 'y' ]; then
+	echo "Adding all files"
+	git add `pwd`/*
+	echo "Commiting with the accepted commit message"
+	git commit -m $msg
+	echo "Pushing the repository to $repo"
+	git push $repo Rewrite
+	echo "Successful"
+else
+	echo "Aborting operation"
+fi
+
